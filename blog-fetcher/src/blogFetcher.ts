c15ae4f57b8ed7276530c6bebeb52f2be46096c0
Lukas <lukas@MBP-von-Lukas.fritz.box>
2023-03-20 18:06:58 +0100

    add redis as in-memory cache between microservices

--- blog-fetcher/src/blogFetcher.ts
+++ blog-fetcher/src/blogFetcher.ts
@@ -2,12 +2,7 @@ import axios from "axios";
 import { Server } from "ws";
 import WebSocket from "ws";
 import { createWordCountMap } from "./wordCounter";
-
-/**
- * Fetch blog posts from the specified API endpoint.
- *
- * @returns {Promise<any[]>} A promise that resolves to an array of blog posts.
- */
+import redisClient from "./redisClient";
 
 export const fetchBlogPosts = async () => {
   try {
@@ -21,13 +16,6 @@ export const fetchBlogPosts = async () => {
   }
 };
 
-/**
- * Broadcast the word count map to all connected WebSocket clients.
- *
- * @param {Server} wss - The WebSocket server.
- * @param {Record<string, number>} wordCountMap - The word count map to broadcast.
- */
-
 export const broadcastWordCountMap = (wss: Server, wordCountMap: Record<string, number>) => {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
@@ -36,15 +24,7 @@ export const broadcastWordCountMap = (wss: Server, wordCountMap: Record<string, 
   });
 };
 
-/**
- * Compare two word count maps for equality.
- *
- * @param {Record<string, number>} map1 - The first word count map.
- * @param {Record<string, number>} map2 - The second word count map.
- * @returns {boolean} True if the maps are equal, false otherwise.
- */
-
- const isEqual = (map1: Record<string, number>, map2: Record<string, number>): boolean => {
+const isEqual = (map1: Record<string, number>, map2: Record<string, number>): boolean => {
   const keys1 = Object.keys(map1);
   const keys2 = Object.keys(map2);
 
@@ -61,12 +41,6 @@ export const broadcastWordCountMap = (wss: Server, wordCountMap: Record<string, 
   return true;
 };
 
-/**
- * Fetch blog posts periodically and send the word count map to WebSocket clients
- * only if the data has changed since the last fetch.
- * @param {Server} wss - The WebSocket server.
- */
-
 export const fetchBlogPostsPeriodically = (wss: Server) => {
   let previousWordCountMap: Record<string, number> = {};
 
@@ -77,23 +51,25 @@ export const fetchBlogPostsPeriodically = (wss: Server) => {
     if (!isEqual(previousWordCountMap, wordCountMap)) {
       console.log("global update - new data recieved")
       broadcastWordCountMap(wss, wordCountMap);
+      await redisClient.set("wordCountMap", JSON.stringify(wordCountMap));
       previousWordCountMap = wordCountMap;
-    } else {console.log("no global update - same data as before")} 
+    } else {
+      console.log("no global update - same data as before")
+    }
   };
 
-  wss.on("connection", (ws) => {
+  wss.on("connection", async (ws) => {
     console.log("Send the lastWordCountMap to Client");
 
-    // Send the lastWordCountMap to the newly connected client
-    if (Object.keys(previousWordCountMap).length > 0) {
-      ws.send(JSON.stringify(previousWordCountMap));
+    const cachedWordCountMap = await redisClient.get("wordCountMap");
+    if (cachedWordCountMap) {
+      ws.send(cachedWordCountMap);
     }
 
     ws.on("close", () => {
       console.log("Client disconnected");
     });
   });
-
 
   processBlogPosts();
   setInterval(processBlogPosts, 10000);

36400e2cf5b3b1ab370bd36d970dfba11ffc94f3
Lukas <lukas@MBP-von-Lukas.fritz.box>
2023-03-20 20:00:04 +0100

    align text center

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography variant="h4" component="h1" gutterBottom textAlign={"center"}>
           WordCountExplorer        
         </Typography>
         <WordCountGrid />

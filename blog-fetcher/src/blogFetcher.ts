99383689c4e5dca227141a3c5b5a74dc53d4709a
Lukas <lukas@MBP-von-Lukas.fritz.box>
2023-03-20 20:45:03 +0100

    add more documentation

--- blog-fetcher/src/blogFetcher.ts
+++ blog-fetcher/src/blogFetcher.ts
@@ -4,6 +4,12 @@ import WebSocket from "ws";
 import { createWordCountMap } from "./wordCounter";
 import redisClient from "./redisClient";
 
+/**
+ * Fetch blog posts from the specified API endpoint.
+ *
+ * @returns {Promise<any[]>} A promise that resolves to an array of blog posts.
+ */
+
 export const fetchBlogPosts = async () => {
   try {
     const response = await axios.get(
@@ -16,6 +22,14 @@ export const fetchBlogPosts = async () => {
   }
 };
 
+/**
+ * Broadcast the word count map to all connected WebSocket clients.
+ *
+ * @param {Server} wss - The WebSocket server.
+ * @param {Record<string, number>} wordCountMap - The word count map to broadcast.
+ */
+
+
 export const broadcastWordCountMap = (wss: Server, wordCountMap: Record<string, number>) => {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
@@ -23,6 +37,14 @@ export const broadcastWordCountMap = (wss: Server, wordCountMap: Record<string, 
     }
   });
 };
+
+/**
+ * Compare two word count maps for equality.
+ *
+ * @param {Record<string, number>} map1 - The first word count map.
+ * @param {Record<string, number>} map2 - The second word count map.
+ * @returns {boolean} True if the maps are equal, false otherwise.
+ */
 
 const isEqual = (map1: Record<string, number>, map2: Record<string, number>): boolean => {
   const keys1 = Object.keys(map1);
@@ -40,6 +62,13 @@ const isEqual = (map1: Record<string, number>, map2: Record<string, number>): bo
 
   return true;
 };
+
+/**
+ * Fetch blog posts periodically and send the word count map to WebSocket clients
+ * only if the data has changed since the last fetch.
+ * @param {Server} wss - The WebSocket server.
+ */
+
 
 export const fetchBlogPostsPeriodically = (wss: Server) => {
   let previousWordCountMap: Record<string, number> = {};
